76fe973f6a33cdaaa3e206e659008b06b60e6efe
Roberto Battaglia <battaroberto@gmail.com>
2019-09-12 19:11:03 +0200

    initialize jupyter notebook

--- indians-diabates/indians-diabetes.py
+++ indians-diabates/indians-diabetes.py
@@ -2,13 +2,15 @@
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
+import matplotlib.pyplot as plt
+
 # load the dataset
 dataset = loadtxt('indians-diabetes.csv', delimiter=',')
 # split into input (X) and output (y) variables
-testX = dataset[0:100:,0:8]
-testy = dataset[0:100:,8]
-X = dataset[100:,0:8]
-y = dataset[100:,8]
+#testX = dataset[0:100:,0:8]
+#testy = dataset[0:100:,8]
+X = dataset[:,0:8]
+y = dataset[:,8]
 # define the keras model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
@@ -19,12 +21,28 @@ model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-model.fit(X, y, epochs=150, batch_size=10)
+history = model.fit(X, y, validation_split=0.25, epochs=200, batch_size=10, verbose=1)
 #evaluate the keras model
-loss, accuracy = model.evaluate(testX, testy)
-print('Accuracy: %.2f, Loss: %.2f' % (accuracy*100, loss*100) )
+#loss, accuracy = model.evaluate(testX, testy)
+#print('Accuracy: %.2f, Loss: %.2f' % (accuracy*100, loss*100) )
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
 # make class predictions with the model
 #predictions = model.predict_classes(testX)
-
-#print('%d tests passed' % passed)
